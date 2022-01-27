48d6a0e175c6406eb64b1c0f25f12d78cf980848
Andri Adam <andriadam2792000@gmail.com>
2022-01-27 10:41:22 +0700

    validation

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -26,13 +26,13 @@
                     <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
                         Halo, {{ Auth::user()->name }}!
                         @if (Auth::user()->avatar)
-                        <img src="{{Auth::user()->avatar}}" class="user-photo" alt="" style="border-radius: 50%">
+                        <img src="{{ Auth::user()->avatar }}" class="user-photo" alt="" style="border-radius: 50%">
                         @else
                         <img src="https://ui-avatars.com/api/?name=Admin" class="user-photo" alt="" style="border-radius: 50%">
                         @endif
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto">
                             <li>
-                                <a href="{{route('dashboard')}}" class="dropdown-item">My Dashboard</a>
+                                <a href="{{ route('dashboard') }}" class="dropdown-item">My Dashboard</a>
                             </li>
                             <li>
                                 <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Sign Out</a>
